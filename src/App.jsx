c12cee5154988b4cc007c903406e1be25a71a53d
Intira Ngernbamrung <intira@aileensolutions.com>
2025-12-23 10:38:36 +0700

    Use HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/aileen-website-sandbox">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
